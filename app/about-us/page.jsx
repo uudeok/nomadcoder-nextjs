3cb96527552ad51fc084fb109be63eff5aa1eb7d
서유덕 <awesome36@daum.net>
2024-07-18 21:15:51 +0900

    prefetch test (7)

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -6,7 +6,7 @@ const Route = ['apple', 'banana', 'strawberry'];
 export default function AboutUs() {
     return (
         <div className={styles.about}>
-            <h1>About us ! </h1>
+            <h1>About us !! </h1>
             {Route.map((route, idx) => (
                 <li key={idx}>
                     <Link href={`/${route}`} prefetch={false}>
